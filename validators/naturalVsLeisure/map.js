82b130f723b7c7f3a06907914961600f71e84904
Rub21 <ruben@mapbox.com>
2016-11-23 15:01:45 -0500

    add test for natural vs leisure

--- validators/naturalVsLeisure/map.js
+++ validators/naturalVsLeisure/map.js
@@ -7,17 +7,14 @@ module.exports = function(tileLayers, tile, writeData, done) {
   var result = [];
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
-   if (val.properties.natural && val.properties.leisure) {
-    val.properties._osmlint = osmlint;
-    result.push(val);
+    if (val.properties.natural && val.properties.leisure) {
+      val.properties._osmlint = osmlint;
+      result.push(val);
     }
   }
-
-
   if (result.length > 0) {
     var fc = turf.featurecollection(result);
     writeData(JSON.stringify(fc) + '\n');
   }
-
   done(null, null);
 };
